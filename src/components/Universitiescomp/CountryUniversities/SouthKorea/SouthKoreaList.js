1717e6988d11a1ec7e7a485fb88735a784ae59c0
Ashish Gautam <ashishgautam9846@gmail.com>
2025-02-11 11:23:40 +0545

    added pages for universities

---
+++ src/components/Universitiescomp/CountryUniversities/SouthKorea/SouthKoreaList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './southkorealist.css'
+const SouthKoreaList = () => {
+  return (
+    <div className='south-container'>
+      <h1>SouthKoreaList</h1>
+      
+      
+      </div>
+  )
+}
+
+export default SouthKoreaList
--- src/components/Universitiescomp/CountryUniversities/SouthKorea/SouthkoreaUni.js
+++ src/components/Universitiescomp/CountryUniversities/SouthKorea/SouthkoreaUni.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from "components/Header/Header";
+import Footer from "components/Header/Footer"
+import SouthKoreaList from './SouthKoreaList';
 
 const SouthkoreaUni = () => {
   return (
-    <div>
+    <div className='main'>
         <Header/>
+        <SouthKoreaList/>
+        <Footer/>
     </div>
   )
 }
